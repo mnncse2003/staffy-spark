b678e0645307df599f215441276aa168f976e062
mnncse2003 <logicaman20@gmail.com>
2026-01-21 17:04:03 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,15 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+        apiKey: "AIzaSyBFHgyqk16_cxG1o7EF2OQ8ksxsjA1ENKk",
+        authDomain: "pq-hub-906ed.firebaseapp.com",
+        databaseURL: "https://pq-hub-906ed-default-rtdb.firebaseio.com",
+        projectId: "pq-hub-906ed",
+        storageBucket: "pq-hub-906ed.appspot.com",
+        messagingSenderId: "226267686237",
+        appId: "1:226267686237:web:6f0583e680ee61cb8534b4",
+        measurementId: "G-NX9Z51PMEJ"
+    };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
